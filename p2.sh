ac50b5b4f012db92a9f33ebe6b621edf554eede5
Quintin <u21513768@tuks.co.za>
2024-06-02 09:32:48 +0200

    Updating main branch
    test8.py - console output for test
    test9.py - xml output for demo

--- p2.sh
+++ p2.sh
@@ -1,4 +1,4 @@
-echo "test" > /home/aa/hello.txt
+echo "start" > /home/aa/error.txt
 
 /usr/bin/python /usr/local/bin/iot/test.py 2>> /home/aa/error.txt
 
--- test4.py
+++
@@ -1,9 +0,0 @@
-import usb.core
-
-# Find all USB devices
-devices = usb.core.find(find_all=True)
-
-# Loop through each device
-for device in devices:
-    # Print device information
-    print(device)
