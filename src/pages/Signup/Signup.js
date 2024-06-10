519b22984285f82cf891133c65b09109edbe2129
Ze-Lin Zhang <u22614495@tuks.co.za>
2024-06-10 20:27:51 +0200

    Added Frontend Structure

---
+++ src/pages/Signup/Signup.js
@@ -0,0 +1,86 @@
+// pages/Signup.js
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Signup.css'; // Import CSS file for styling
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here (e.g., send data to server)
+    console.log('Form submitted:', formData);
+  };
+
+  return (
+    <div className="auth-container">
+      <div className="auth-form">
+        <h2>Sign up</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit" className="btn-submit">Sign up</button>
+        </form>
+        <p>Already have an account? <Link to="/login">Login</Link></p>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
