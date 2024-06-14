70db057de6cd591f571f55d966c9b2c5c3950a01
Quintin <u21513768@tuks.co.za>
2024-06-14 20:22:18 +0200

    Update upload_to_server.py

--- src/upload_to_server.py
+++ src/upload_to_server.py
@@ -7,12 +7,12 @@ uri = "mongodb+srv://fluffythedragonslayer:RckbSNrFQpgO2cV2@cluster0.qisokbd.mon
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+# # Send a ping to confirm a successful connection
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
 
 # Define the database and collection
 db = client['file_upload']
