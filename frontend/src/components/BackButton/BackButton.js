2586a9d133c6f2c97bf198110625ab4184428eeb
Ze-Lin Zhang <u22614495@tuks.co.za>
2024-06-22 22:11:57 +0200

    Add Navigation buttons

---
+++ frontend/src/components/BackButton/BackButton.js
@@ -0,0 +1,20 @@
+// BackButton.js
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './BackButton.css';
+
+const BackButton = () => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
+  return (
+    <button className="back-button" onClick={handleBackClick}>
+      ←
+    </button>
+  );
+};
+
+export default BackButton;
