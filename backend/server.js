86ff5ad069e42691ddfac1cf920128f435de5960
Chanseop Carter Shin <117119386+CarterShin@users.noreply.github.com>
2024-06-23 18:15:00 +0200

    hotfix server

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
