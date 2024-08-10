0c770748f1cd2c864decf0c87656660121de9193
Quintin <u21513768@tuks.co.za>
2024-08-10 15:33:14 +0200

    Access Iot device correctly
    Allows for accessing iot device scripts correctly

--- frontend/src/pages/RaspberryPi/RaspberryPi.js
+++ frontend/src/pages/RaspberryPi/RaspberryPi.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import IoT_Device from '../../components/IOT_DEVICE/IOT_DEVICE';
 import './RaspberryPi.css';
+import Cookies from 'js-cookie'; // Make sure to import this
 
 function RaspberryPi() {
+  const deviceId = Cookies.get('deviceId') || ''; // Retrieve deviceId from cookies
+  console.log('deviceId:', deviceId); // This will log the deviceId retrieved from cookies
+
   return (
     <div className="raspberry-pi">
       <Header />
@@ -11,11 +15,12 @@ function RaspberryPi() {
         <h1 className="devices-title">IoT Devices</h1>
         <p><small style={{ color: '#B7B5B7'}}><span style={{color: 'white'}}>HINT:</span> Don't forget to consult our guide if you need help or get stuck <span style={{color: 'white'}}>*<span style={{color: '#007BFF'}}>blue icon</span> - bottom right</span></small></p>
         <hr />
-        {/* eslint-disable-next-line react/jsx-pascal-case */}
-        <IoT_Device />
+        {/* Pass the deviceId to IoT_Device */}
+        <IoT_Device deviceId={deviceId} />
       </div>
     </div>
   );
 }
 
 export default RaspberryPi;
+
