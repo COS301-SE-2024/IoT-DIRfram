f9f1a02774980a8e09bbd188d52d608bb610579e
lloydcreighton <lloydcreighton7@gmail.com>
2024-09-24 14:12:18 +0200

    Added some new signup tests

--- frontend/src/tests/playwright/signup.spec.js
+++ frontend/src/tests/playwright/signup.spec.js
@@ -1,5 +1,3 @@
-//COMMENTED OUT BECAUSE I DONT WANT TO CREATE NEWQ USERS THE WHOLE TIME
-
 const { test, expect } = require('@playwright/test');
 
 test.describe('Signup Flow', () => {
@@ -27,5 +25,29 @@ test.describe('Signup Flow', () => {
     await page.click('button[type="submit"]');
 
     await expect(page.locator('text=Username is already taken')).toBeVisible();
+  });
+
+  test('shows validation errors with taken email', async ({ page }) => {
+    await page.goto('http://localhost:3000/signup');
+
+    await page.fill('input[name="username"]', 'newuser');
+    await page.fill('input[name="email"]', 'dagantheking@gmail.com');
+    await page.fill('input[name="password"]', 'Password123');
+    await page.fill('input[name="confirmPassword"]', 'Password123');
+
+    await page.click('button[type="submit"]');
+    await expect(page.locator('text=Email is already taken').first()).toBeVisible();
+  });
+
+  test('shows validation errors with mismatched passwords', async ({ page }) => {
+    await page.goto('http://localhost:3000/signup');
+
+    await page.fill('input[name="username"]', 'newuser');
+    await page.fill('input[name="email"]', 'email@email.com');
+    await page.fill('input[name="password"]', 'Password123');
+    await page.fill('input[name="confirmPassword"]', 'Password1234');
+
+    await page.click('button[type="submit"]');
+    await expect(page.locator('text=Passwords do not match').first()).toBeVisible();
   });
 });
